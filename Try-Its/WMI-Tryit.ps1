86a82f93ca3d50216e70171b075a2e449d244aed
Matt Johnson <mjohnson@mwjcomputing.com>
2017-10-09 13:26:44 -0400

    Update to WMI Try It

--- Try-Its/WMI-Tryit.ps1
+++ Try-Its/WMI-Tryit.ps1
@@ -6,3 +6,13 @@ Start-Process 'notepad.exe'
 $Notepad = Get-CimInstance -ClassName Win32_Process -Filter "Name = 'notepad.exe'"
 Invoke-CimMethod -InputObject $Notepad -MethodName Terminate
 
+# Number 3
+(Get-CimClass -ClassName Win32_Process).CimClassMethods
+
+# Number 4
+$Arguments = @{
+    CommandLine = 'notepad.exe';
+    CurrentDirectory = $null;
+    ProcessStartupInformation = $null
+}
+Invoke-CimMethod -ClassName Win32_Process -MethodName Create -Arguments $Arguments
